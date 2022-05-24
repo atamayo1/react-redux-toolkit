f9a6de3dc46736cc93754407d86a06730f56e430
atamayo1 <anthony1_144@hotmail.com>
2022-05-24 01:46:59 -0500

    first commit

---
+++ react-redux-app/src/App.js
@@ -0,0 +1,15 @@
+import { Provider } from "react-redux";
+import Navbar from "./components/Navbar";
+import UserList from "./components/UserList";
+import store from "./store";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Navbar />
+      <UserList />
+    </Provider>
+  );
+}
+
+export default App;
